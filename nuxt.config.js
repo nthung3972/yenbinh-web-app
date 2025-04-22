b820affdf4d08ff46a7c7a5c46c63f9b3cf4006d
nthung <nthung.nvit.yenbinh@gmail.com>
2025-04-22 07:09:22 -0700

    test3

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 export default defineNuxtConfig({
+  ssr: false,
   css: [
     '~/assets/css/style.css',
     'bootstrap/dist/css/bootstrap.min.css'
--- plugins/api.js
+++ plugins/api.js
@@ -3,6 +3,7 @@ import ApiService from "~/services/api.service";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const config = useRuntimeConfig(); // Lấy biến môi trường đúng cách
+  console.log("API URL:", config.public.apiUrl)
 
   // Khởi tạo ApiService với URL từ runtimeConfig
   ApiService.init({
